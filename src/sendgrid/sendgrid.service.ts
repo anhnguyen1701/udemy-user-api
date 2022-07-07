5cfa047225dc8d760a141f3f9ecbbe229ce8cebe
anhnguyen <nguyenlamanh1701@gmail.com>
2022-07-07 14:09:48 +0700

    send mail ses

--- src/sendgrid/sendgrid.service.ts
+++ src/sendgrid/sendgrid.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as sgMail from '@sendgrid/mail';
+import * as nodemailer from 'nodemailer';
 
 @Injectable()
 export class SendgridService {
@@ -9,5 +10,41 @@ export class SendgridService {
 
   public async send(data: sgMail.MailDataRequired): Promise<any> {
     return sgMail.send(data);
+  }
+
+  public async sendMailSes(email: string) {
+    const smtpEndpoint = 'email-smtp.us-east-2.amazonaws.com'; // servername
+    const port = '587'; // smtp port
+    const senderAddress = 'nguyenlamanh1701@gmail.com'; // email sender
+    const toAddresses = email; // email recevie
+    const smtpUsername = 'AKIAQFWP7NFWNNNGT24D'; // smtp username
+    const smtpPassword = 'BPbtgTKg3C7IBMe9nvDQblmn+qzCFDK94dyQi9CB7KTg'; //smpt password
+
+    //init a transporter to send mail
+    const transporter = nodemailer.createTransport({
+      host: smtpEndpoint,
+      port: port,
+      auth: {
+        user: smtpUsername,
+        pass: smtpPassword,
+      },
+    });
+
+    const otpCode = Math.floor(100000 + Math.random() * 900000);
+
+    //mail options
+    const options = {
+      from: senderAddress,
+      to: toAddresses,
+      subject: 'OTP verification',
+      text: 'Your OTP code verification is ' + otpCode,
+    };
+
+    try {
+      const res = await transporter.sendMail(options);
+      console.log('send mail ' + res);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,13 +2,22 @@ import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { SendgridService } from 'src/sendgrid/sendgrid.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly sendGridService: SendgridService,
+  ) {}
 
   @Post('/create')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Get('/send-mail-ses')
+  async test(@Body('email') email: string) {
+    return await this.sendGridService.sendMailSes(email);
   }
 }
