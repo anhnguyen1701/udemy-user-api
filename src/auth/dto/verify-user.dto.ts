652b36001bb8157060a9d114915549030c7cac73
anhnguyen <nguyenlamanh1701@gmail.com>
2022-07-11 10:26:27 +0700

    add validation for dto

--- src/auth/dto/verify-user.dto.ts
+++ src/auth/dto/verify-user.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class VerifyUserDto {
   @ApiProperty({ description: 'email' })
+  @IsEmail()
   readonly email: string;
 
   @ApiProperty({ description: 'otp' })
+  @IsNotEmpty()
   readonly otp: number;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,10 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ description: 'Email' })
+  @IsEmail()
   readonly email: string;
+
   @ApiProperty({ description: 'Password' })
+  @IsNotEmpty()
   readonly password: string;
+
   @ApiProperty({ description: 'FUll name' })
+  @IsNotEmpty()
   readonly fullname: string;
 }
